eba7c16765eca890426b46c202a80ea14fd6049c
skykanin <3789764+skykanin@users.noreply.github.com>
2018-10-19 13:19:34 +0200

    Implemented Account actor

--- task2-project/src/main/scala/Account.scala
+++ task2-project/src/main/scala/Account.scala
@@ -24,21 +24,31 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
 
     def getTransactions: List[Transaction] = {
         // Should return a list of all Transaction-objects stored in transactions
-        ???
+        transactions.toList.map { case (_, transaction) => transaction }
     }
 
     def allTransactionsCompleted: Boolean = {
         // Should return whether all Transaction-objects in transactions are completed
-        ???
+        getTransactions forall { _.isCompleted }
     }
 
-    def withdraw(amount: Double): Unit = ??? // Like in part 2
-    def deposit(amount: Double): Unit = ??? // Like in part 2
-    def getBalanceAmount: Double = ??? // Like in part 2
+    def withdraw(amount: Double): Unit = amount match {
+        case `amount` if `amount` < 0 => throw new IllegalAmountException("You can't withdraw negative amounts")
+        case `amount` if `amount` > getBalanceAmount => throw new NoSufficientFundsException("Withdrawal too big")
+        case _ => balance.synchronized { balance.amount -= amount }
+    }
+
+    def deposit(amount: Double): Unit = amount match {
+        case `amount` if `amount` < 0 => throw new IllegalAmountException("You can't deposit negative amounts")
+        case _ => balance.synchronized { balance.amount += amount }
+    }
+
+    def getBalanceAmount: Double = balance.synchronized { balance.amount }
 
     def sendTransactionToBank(t: Transaction): Unit = {
         // Should send a message containing t to the bank of this account
-        ???
+        val bankRef: ActorRef = BankManager.findBank(bankId)
+        bankRef ! t
     }
 
     def transferTo(accountNumber: String, amount: Double): Transaction = {
@@ -55,9 +65,7 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
                     t.status = TransactionStatus.FAILED
             }
         }
-
         t
-
     }
 
     def reserveTransaction(t: Transaction): Boolean = {
@@ -68,23 +76,40 @@ class Account(val accountId: String, val bankId: String, val initialBalance: Dou
       false
     }
 
-    override def receive = {
-		case IdentifyActor => sender ! this
+    override def receive: PartialFunction[Any, Unit] = {
+        case IdentifyActor => sender ! this
 
-		case TransactionRequestReceipt(to, transactionId, transaction) => {
-			// Process receipt
-			???
-		}
+        case TransactionRequestReceipt(to, transactionId, transaction) => to match {
+            // Process receipt
+            case `to` if `to` == getFullAddress =>
+                val temp = transactions(transactionId)
+                temp.status = transaction.status
+        }
 
-		case BalanceRequest => ??? // Should return current balance
+        case BalanceRequest => getBalanceAmount // Should return current balance
 
-		case t: Transaction => {
-			// Handle incoming transaction
-			???
-		}
+        case t: Transaction => {
+          // Handle incoming transaction
+          transactions += (t.id -> t)
 
-		case msg => ???
+            getTransactions foreach {t =>
+                if (t.to == getFullAddress) {
+                    try {
+                        deposit(t.amount)
+                        t.status = TransactionStatus.SUCCESS
+                    } catch {
+                        case _: IllegalAmountException =>
+                            t.status = TransactionStatus.FAILED
+                            val bankId = t.from.dropRight(4)
+                            BankManager.findBank(bankId) ! TransactionRequestReceipt(t.from, t.id, t)
+                    }
+                } else {
+                    transferTo(t.to, t.amount)
+                }
+            }
+        }
+
+        case msg => ???
     }
-
 
 }
